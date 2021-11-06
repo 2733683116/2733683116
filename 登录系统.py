cf7ff1a4f57a34162781da1a7366c31405a8ccd5
2733683116 <92662454+2733683116@users.noreply.github.com>
2021-11-07 06:41:54 +0800

    Update 登录系统.py

--- 登录系统.py
+++ 登录系统.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox
 import pickle
-
-
 window = tk.Tk()
 window.title('欢迎进入学习系统，希望你使用愉快')
 window.geometry('450x300')
@@ -25,12 +23,10 @@ def usr_in():
     usr_name = var_usr_name.get()
     usr_pwd = var_usr_pwd.get()
     try:
-        with open('usr_info.pickle', 'rb') as usr_file:
-            usr_info = pickle.load(usr_file)
-    except FileExistsError:
-        with open('usr_info.pickle', 'wb') as usr_file:
-            usrs_info = {'admin': 'admin'}
-            pickle.dump(usrs_info, usr_file)
+        usr_files = open('usr_info.text', 'r')
+        usr_file_ = pickle.load(usr_files)
+    except:
+        pass
     f = 1
     for i in usr_name:
         if 255 >= ord(i) >= 128:
@@ -39,8 +35,8 @@ def usr_in():
     for ii in usr_pwd:
         if 255 >= ord(ii) >= 128:
             k = 0
-    if usr_name in usr_info:
-        if usr_pwd == usr_info[usr_name]:
+    if usr_name in usr_file_:
+        if usr_pwd == usr_file_[usr_name]:
             tk.messagebox.showinfo(title='Welcome', message='欢迎您：' + usr_name)
         else:
             tk.messagebox.showinfo(message='密码错误')
@@ -58,6 +54,16 @@ def usr_in():
         tk.messagebox.showinfo(message='密码内不可存在中文')
     else:
         is_signup = tk.messagebox.askyesno('欢迎', '系统检测到您还未有注册， 是否现在注册？')
+        if is_signup == True:
+            usr_up()
+        else:
+            usr_out()
+
+
+def usr_out():
+    out = tk.messagebox.askyesno('我还会想你的', '是否确认退出')
+    if out == True:
+        usr_sign_quit()
 
 
 def usr_up():
@@ -74,38 +80,39 @@ def usr_up():
             if 255 >= ord(ii) >= 128:
                 k = 0
         try:
-            with open('usr_info.pickle', 'rb') as usr_file:
-                exist_usr_info = pickle.load(usr_file)
+            usr_files = open('usr_info.text', 'rb')
+            exist_usr_info = pickle.load(usr_files)
         except FileExistsError:
             exist_usr_info = {}
         if nn in exist_usr_info:
             tk.messagebox.showerror('注册错误', '该用户名已存在，无法注册，请登录')
+            usr_up()
         elif nn == '':
             tk.messagebox.showerror('注册失败', '用户名内未检测到数据')
+            usr_up()
         elif np == '':
             tk.messagebox.showerror('注册失败', '密码内未检测到数据')
+            usr_up()
         elif f == 0:
             tk.messagebox.showerror('注册失败', '用户名不可存在中文')
+            usr_up()
         elif k == 0:
             tk.messagebox.showerror('注册失败', '密码不可存在中文')
+            usr_up()
         elif np != npf:
             tk.messagebox.showerror('注册失败', '密码前后不一致')
+            usr_up()
         else:
             exist_usr_info[nn] = np
-            with open('usr_info.pickle', 'wb') as usr_file:
-                pickle.dump(exist_usr_info, usr_file)
+            usr_files = open('usr_info.text', 'w')
+            usr_file = usr_files.write('nn:np')
+            usr_files.close()
             tk.messagebox.showinfo('欢迎', '注册成功!恭喜你成为学习系统的一员!!!')
             window_sign_up.destroy()
 
     window_sign_up = tk.Toplevel(window)
     window_sign_up.geometry('350x200')
     window.title('学习系统注册')
-    window.geometry('450x300')
-    tk.Canvas(window, heigh=400, width=300)
-    canvas = tk.Canvas(window, height=300, width=500)
-    image_file = tk.PhotoImage(file=r'D:\python代码区\照片素材\001.png')
-    image = canvas.create_image(0, 0, anchor='nw', image=image_file)
-    canvas.pack(side='top')
     new_name = tk.StringVar()
     tk.Label(window_sign_up, text='用户名： ').place(x=10, y=10)
     tk.Entry(window_sign_up, textvariable=new_name).place(x=150, y=10)
@@ -115,18 +122,19 @@ def usr_up():
     new_pwd_confirm = tk.StringVar()
     tk.Label(window_sign_up, text='请再次输入密码： ').place(x=10, y=90)
     tk.Entry(window_sign_up, textvariable=new_pwd_confirm, show='*').place(x=150, y=90)
-    bt_confirm_sign_up = tk.Button(window_sign_up, text='确认注册', command=sign_cg)
+    bt_confirm_sign_up = tk.Button(window_sign_up, text='确认注册', command=sign_cg, bg='red')
     bt_confirm_sign_up.place(x=150, y=130)
 
 
-def usr_sign_quit():
-    window.destroy()
+def usr_sign_quit(wandow=window):
+    wandow.update()
+    wandow.destroy()
 
 
-bt_in = tk.Button(window, text='登录', command=usr_in)
+bt_in = tk.Button(window, text='登录', command=usr_in, bg='red')
 bt_in.place(x=140, y=230)
-bt_up = tk.Button(window, text='注册', command=usr_up)
+bt_up = tk.Button(window, text='注册', command=usr_up, bg='blue')
 bt_up.place(x=210, y=230)
-bt_quit = tk.Button(window, text='退出', command=usr_sign_quit)
+bt_quit = tk.Button(window, text='退出', command=usr_out, bg='grey')
 bt_quit.place(x=280, y=230)
 window.mainloop()
