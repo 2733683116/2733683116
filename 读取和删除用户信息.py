2ab0a51136201df1a1cc7ed7045022b923296308
2733683116 <92662454+2733683116@users.noreply.github.com>
2021-11-07 15:30:43 +0800

    Create 读取和删除用户信息.py

---
+++ 读取和删除用户信息.py
@@ -0,0 +1,14 @@
+'''
+配合用户界面一起用
+'''
+import pickle
+with open('./usr_info.pickle', 'rb') as usr_file:
+    usr_info = pickle.load(usr_file)
+print(usr_info)
+usr = input('请输入需要删除的用户')
+usr_info.pop(usr)
+with open('usr_info.pickle', 'wb') as usr_files:
+    usrs_info = usr_info
+    usr_info = pickle.dump(usrs_info, usr_files)
+print('已经成功删除', usr)
+print('还剩下', usrs_info)
